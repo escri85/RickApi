273d4af5b93c95aafccdfc5bace06bb6bcc7a0a8
Sergio Escribano Fernandez <escri85@gmail.com>
2022-01-30 20:13:38 +0100

    modifico la busqueda por nombre y añado por id

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Axios from "axios";
 function App() {
   const [characterID, setCharacterId] = useState("");
   const [characterChosen, setCharacterChosen] = useState(false);
+
   const [character, setCharacter] = useState({
     id: "",
     name: "",
@@ -16,18 +17,34 @@ function App() {
   });
 
   const searchRick = () => {
-    Axios.get(`https://rickandmortyapi.com/api/character/${characterID}`).then(
-      (res) =>
-      setCharacter({
-          id: characterID,
-          name: res.data.name,
-          image: res.data.image,
-          gender: res.data.gender,
-          species: res.data.species,
-          status: res.data.status,
-        }),
-      setCharacterChosen(true)
-    );
+    Axios.get(`https://rickandmortyapi.com/api/character/?name=${characterID}`).then(
+      (res) => {/* console.log(res.data.results); */
+        const result = res.data.results
+        result.map((item)=>{
+          return(setCharacter({
+            id: item.id,
+        name: item.name,
+        image: item.image,
+        gender: item.gender,
+        species: item.species,
+        status: item.status,
+          }),setCharacterChosen(true))
+          
+        })
+      }
+
+
+      // setCharacter({
+
+      //   id: characterID,
+      //   name: res.data.name,
+      //   image: res.data.image,
+      //   gender: res.data.gender,
+      //   species: res.data.species,
+      //   status: res.data.status,
+      // }),
+      // setCharacterChosen(true),
+    )
   };
   //  const searchRick = () => {
   //   Axios.get(`https://rickandmortyapi.com/api/character/?name=${characterID}`).then(
@@ -52,19 +69,31 @@ function App() {
           onChange={(event) => {
             setCharacterId(event.target.value);
           }}
-          // value={characterID.toLowerCase()}
+          value={characterID.toLowerCase()}
         />
+
         <Boton onClick={searchRick}>Search</Boton>
+       {/*  <Boton>humano</Boton>
+        <Boton>alive</Boton>
+        <Boton>alien</Boton>
+        <Boton>death</Boton> */}
+
+
       </div>
       <div className="card">
-        {!characterChosen ? <h1>elige un id</h1> : (
-        <>
-        <h1> Nombre:{character.name}</h1>
-        <p>Identificador:{character.id}</p>
-        <p>Genero:{character.gender}</p>
-        <p>Especie{character.species}</p>
-          <img src={character.image} alt="" />
-        </>)}
+        {!characterChosen ? (
+          <h1>elige un id</h1>
+        ) : (
+          <>
+            <h1> Nombre:{character.name}</h1>
+            <p>Identificador:{character.id}</p>
+            <p>Genero:{character.gender}</p>
+            <p>Especie{character.species}</p>
+            <p>Especie{character.status}</p>
+
+            <img src={character.image} alt="" />
+          </>
+        )}
       </div>
     </div>
   );
